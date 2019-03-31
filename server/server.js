a11cf85069144004a730915c795cb89bf205808c
toskatlt <to_Ska@mail.ru>
2019-03-31 10:25:15 +0400

    v 0.9

--- server/server.js
+++ server/server.js
@@ -10,122 +10,162 @@ const pool = mysql.createPool({
 	port: '3306'
 });
 
+const app = express();
 
-pool.getConnection((err, connection) => {
-	if (err) {
-		throw err;
-	} else {
-		console.log('MySQL Connected...');
-	}
+// SELECT
 
-	const app = express();
-
-	// SELECT
-
-	app.get('/api/products/', (req, res) => {
-		let sql = `SELECT * FROM products LIMIT ${req.query.from} , ${req.query.to}`;
-		let query = connection.query(sql, (err, result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	app.get('/api/products/:id', (req, res) => {
-		let sql = `SELECT * FROM products WHERE id = ${req.params.id}`;
-		let query = connection.query(sql, (err,result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	app.get('/api/cart/', (req, res) => {
-		let sql = `SELECT products.*, cart.quantity FROM cart, products WHERE cart.id = products.id`;
-		let query = connection.query(sql, (err, result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	app.get('/api/cart/:id', (req, res) => {
-		let sql = `SELECT quantity FROM cart WHERE id = ${req.params.id}`;
-		let query = connection.query(sql, (err,result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	app.get('/api/cartCount', (req, res) => {
-		let sql = `SELECT count(*) as count FROM cart`;
-		let query = connection.query(sql, (err,result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	// POST
-
-	app.post('/api/cart/:id', (req, res) => {
-		let sql = `INSERT INTO cart (id, quantity) VALUES (${req.params.id}, 1)`;
-		let query = connection.query(sql, (err,result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	// UPGRADE
-
-	app.put('/api/cart/:id', (req, res) => {
-		let sql = `UPDATE cart SET quantity = ${req.query.quantity} WHERE id= ${req.params.id}`;
-		let query = connection.query(sql, (err,result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	// DELETE
-
-	app.delete('/api/cart/:id', (req, res) => {
-		let sql = `DELETE FROM cart WHERE id = ${req.params.id}`;
-		let query = connection.query(sql, (err,result) => {
-			if (err) {
-				return res.sendStatus(404, JSON.springify({result: 0, text: err}))
-			} else {
-				return res.send(result);
-			}
-			connection.release();
-		})
-	});
-
-	app.listen(3000, () => {
-		console.log('Server started on port 3000');
+app.get('/api/products/', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `SELECT * FROM products LIMIT ${req.query.from} , ${req.query.to}`;
+			let query = connection.query(sql, (err, result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+		connection.release();
 	})
+});
+
+
+app.get('/api/products/:id', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `SELECT * FROM products WHERE id = ${req.params.id}`;
+			let query = connection.query(sql, (err,result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+		connection.release();
+	})
+});
+
+app.get('/api/cart/', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `SELECT products.*, cart.quantity FROM cart, products WHERE cart.id = products.id`;
+			let query = connection.query(sql, (err, result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+		connection.release();
+	})
+});
+
+app.get('/api/cart/:id', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `SELECT quantity FROM cart WHERE id = ${req.params.id}`;
+			let query = connection.query(sql, (err,result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+		connection.release();
+	})
+});
+
+app.get('/api/cartCount', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `SELECT count(*) as count FROM cart`;
+			let query = connection.query(sql, (err,result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+		connection.release();
+	})
+});
+
+// POST
+
+app.post('/api/cart/:id', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `INSERT INTO cart (id, quantity) VALUES (${req.params.id}, 1)`;
+			let query = connection.query(sql, (err,result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+	    connection.release();
+	})
+});
+
+// UPGRADE
+
+app.put('/api/cart/:id', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `UPDATE cart SET quantity = ${req.query.quantity} WHERE id= ${req.params.id}`;
+			let query = connection.query(sql, (err,result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+		connection.release();
+	})
+});
+
+// DELETE
+
+app.delete('/api/cart/:id', (req, res) => {
+	pool.getConnection((err, connection) => {
+		if (err) {
+			throw err;
+		} else {
+			let sql = `DELETE FROM cart WHERE id = ${req.params.id}`;
+			let query = connection.query(sql, (err,result) => {
+				if (err) {
+					return res.sendStatus(404, JSON.springify({result: 0, text: err}))
+				} else {
+					return res.send(result);
+				}
+			})
+		}
+		connection.release();
+	})
+});
+
+app.listen(3000, () => {
+	console.log('Server started on port 3000');
+})
 	
-});	
